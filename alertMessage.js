e4e90ba2ecbb743ab4095b9531f83c9bb6649e14
Megan Durham <megan@mobiusconsortium.org>
2015-04-09 13:02:37 -0500

    added js and styling for message until sunday

---
+++ alertMessage.js
@@ -0,0 +1,16 @@
+$( document ).ready(function() {
+var now = new Date();
+
+var sunday = new Date("April 12, 2015");
+
+if(now < sunday)
+{
+  $('<p>Due to scheduled maintenance, the catalog will be unavailable beginning Saturday, April 11 at 10pm CST until Sunday, April 12 at 2am CST.</p>').appendTo('#alertMessage');   
+  $('#alertMessage').css({"margin":"auto", "font-size":"1.2em", "color":"black", "text-align":"center", "padding":"1em", "background-color":"lightpink"});
+  $('#alertMessage').show();
+}
+else if (now > sunday)
+{
+  $('#alertMessage').hide();
+}  
+});
