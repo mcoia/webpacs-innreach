0224ffb69af5766e0bf80a07f3b74e1ef5c38318
Megan Durham <megan@mobiusconsortium.org>
2015-04-09 14:21:44 -0500

    added alertmsg to styles.css instead of alertmsg.js

--- alertMessage.js
+++ alertMessage.js
@@ -6,7 +6,7 @@ var sunday = new Date("April 12, 2015");
 if(now < sunday)
 {
   $('<p>Due to scheduled maintenance, the catalog will be unavailable beginning Saturday, April 11 at 10pm CST until Sunday, April 12 at 2am CST.</p>').appendTo('#alertMessage');   
-  $('#alertMessage').css({"margin":"auto", "font-size":"1.2em", "color":"black", "text-align":"center", "padding":"1em", "background-color":"lightpink"});
+  //$('#alertMessage').css({"margin":"auto", "font-size":"1.2em", "color":"black", "text-align":"center", "padding":"1em", "background-color":"lightpink"});
   $('#alertMessage').show();
 }
 else if (now > sunday)
