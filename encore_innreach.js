ba886a0d1f38fe6f79b8456e272047945798801c
Megan Durham <megan@mobiusconsortium.org>
2015-08-18 11:25:09 -0500

    testing to see if we can add js file to encore

---
+++ encore_innreach.js
@@ -0,0 +1,4 @@
+jQuery(document).ready(function(){
+	jQuery("img[id*='searchResultThumbnailImageAny2Component']" ).addClass('syn_skipthis');
+	jQuery("img[id*='searchResultThumbnailImageAnyComponent']" ).addClass('syn_skipthis');
+});
